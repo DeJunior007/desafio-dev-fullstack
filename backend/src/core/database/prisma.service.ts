5dea64a834f889256e2036da2638fccd57f4e2d6
DeJunior007 <deiltonp74@gmail.com>
2026-02-17 03:36:21 -0300

    feat: adiciona módulo e service de conexão com banco via prisma

---
+++ backend/src/core/database/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+
+    super({
+      adapter,
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('🔌 Banco conectado via Driver Adapter');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
